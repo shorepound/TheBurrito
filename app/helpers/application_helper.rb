bb2e236d6cfcd666348d8cd46d67c010837d9255
Nathan Arnold <shorepound01@gmail.com>
2025-12-27 20:43:04 -0600

    add partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def card_taqueria(extra_classes = "", &block)
+		classes = ["card-taqueria", extra_classes].join(" ").strip
+		content_tag(:div, class: classes) do
+			capture(&block)
+		end
+	end
 end
